db90144bd4085e0425bb0fee8f7c9536930b6809
Luc205299 <94066059+Luc205299@users.noreply.github.com>
2024-02-19 23:16:22 +0100

    ui"et"iuygrze

---
+++ src/Newpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NewPage() {
+  return (
+    <div>
+      <h1>New Page</h1>
+      <p>This is the content of the new page.</p>
+    </div>
+  );
+}
+
+export default NewPage;
