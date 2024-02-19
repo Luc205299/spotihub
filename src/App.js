424d88bad61c1f4da1d239ab5cf4ed4296fdfde6
Luc205299 <94066059+Luc205299@users.noreply.github.com>
2024-02-19 21:52:16 +0100

    ajour page acceuil en react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; 
+import logo from './logo.png';
+
 
 function App() {
+  const pageAuth = () => {
+    window.location.href = "redirect.html";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="logo">
+        Spotyhub
+        <img src={logo} alt="Logo" className="logo" width="200" height="200" />
+      </div>
+      <h1 className="title">Discover a new way to listen to music with friends!</h1>
+      <button className="button" onClick={pageAuth}>Try it</button>
     </div>
   );
 }
